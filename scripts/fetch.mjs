ab0365be8649f5d635bc5cabf9c4d9a22594c8ae
sunya9 <mugen.xyz@gmail.com>
2021-12-11 20:55:00 +0900

    fix: use zx

---
+++ scripts/fetch.mjs
@@ -0,0 +1,16 @@
+#!/usr/bin/env zx
+
+import "zx/globals";
+
+const rootDir = path.resolve(__dirname, "../");
+cd(rootDir);
+
+await fetch(
+  `${process.env.GHOST_URL}/ghost/api/v3/content/settings?key=${process.env.GHOST_KEY}`
+)
+  .then((res) => res.text())
+  .then((res) => fs.writeFile("site.json", res));
+
+await fetch(`${process.env.GHOST_URL}/rss`)
+  .then((res) => res.text())
+  .then((res) => fs.writeFile("public/rss.xml", res));
--- scripts/fetch.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -u
-
-curl -sL $GHOST_URL/ghost/api/v3/content/settings?key=$GHOST_KEY -o site.json
-curl -sL $GHOST_URL/rss -o public/rss.xml
