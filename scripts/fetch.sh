f2e7fa9fbcd6225c9ff3d06a44a45644a76ba698
sunya9 <mugen.xyz@gmail.com>
2021-12-11 20:02:58 +0900

    fix: use curl instead of wget

--- scripts/fetch.sh
+++ scripts/fetch.sh
@@ -2,5 +2,5 @@
 
 set -u
 
-wget -q $GHOST_URL/ghost/api/v3/content/settings?key=$GHOST_KEY -O site.json
-wget -q $GHOST_URL/rss -O public/rss.xml
+curl -sL $GHOST_URL/ghost/api/v3/content/settings?key=$GHOST_KEY -o site.json
+curl -sL $GHOST_URL/rss -o public/rss.xml
