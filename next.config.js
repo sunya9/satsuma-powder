b12e1e0a8e3ca0493b2c879e5d8cdd526b0978b4
sunya9 <mugen.xyz@gmail.com>
2022-03-04 01:55:58 +0900

    fix: move rewirtes/redirects configuration to vercel.json

--- next.config.js
+++ next.config.js
@@ -5,12 +5,4 @@ module.exports = {
   env: {
     NEXT_PUBLIC_GA_ID: "UA-10104011-9",
   },
-  rewrites: () => [{ source: "/rss/", destination: "/rss.xml" }],
-  redirects: () => [
-    {
-      source: "/:year(\\d{4,})/:month(\\d{2,})/:day(\\d{2,})/:slug",
-      destination: "/blog/:slug",
-      permanent: true,
-    },
-  ],
 };
