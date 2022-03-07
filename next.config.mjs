2872a3fd6e24dcb8bd9e069c5245f26aa100277e
sunya9 <mugen.xyz@gmail.com>
2022-03-08 06:38:09 +0900

    fix: use esm (type: module)

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const config = {
   reactStrictMode: true,
   trailingSlash: true,
   env: {
     NEXT_PUBLIC_GA_ID: "UA-10104011-9",
   },
 };
+
+export default config;
