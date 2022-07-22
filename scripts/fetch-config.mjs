33926992742f8b6a4b4203ac95c9badcada4eafa
sunya9 <mugen.xyz@gmail.com>
2022-07-22 19:25:47 +0900

    fix: import functions from zx explicitly

--- scripts/fetch-config.mjs
+++ scripts/fetch-config.mjs
@@ -1,12 +1,12 @@
 #!/usr/bin/env zx
 
-import "zx/globals";
+import { $, cd, path, fs } from "zx";
 
 $.verbose = false;
 
 cd(path.resolve(__dirname, "../"));
 
-const res = await fetch(
+await fetch(
   `${process.env.GHOST_URL}/ghost/api/v3/content/settings?key=${process.env.GHOST_KEY}`
 )
   .then((res) => res.text())
