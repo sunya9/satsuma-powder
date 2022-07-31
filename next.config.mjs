e6bbaa0b95921e22c1c22305fbc2ce5f74e4f817
sunya9 <mugen.xyz@gmail.com>
2022-08-01 05:55:53 +0900

    fix: stop using trailing slash

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
-  trailingSlash: true,
+  trailingSlash: false,
   env: {
     NEXT_PUBLIC_GA_ID: "UA-10104011-9",
   },
