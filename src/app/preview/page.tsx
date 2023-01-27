2666476857f61cb058fa42e70fb4c3cdfaa6467b
sunya9 <mugen.xyz@gmail.com>
2023-01-28 08:19:52 +0900

    fix: PreviewData type

--- src/app/preview/page.tsx
+++ src/app/preview/page.tsx
@@ -1,18 +1,21 @@
+import { PreviewData } from "next";
 import { previewData } from "next/headers";
 import { AppLayout } from "../../componments/AppLayout";
 import { Article } from "../../componments/Article";
 import { ghostRepo } from "../../lib/ghost";
 import { notFound } from "next/navigation";
 
-type PreviewData =
-  | {
-      uuid: string;
-    }
-  | undefined;
+type AppPreviewData = {
+  uuid: string;
+};
+
+function isAppPreviewData(obj: PreviewData): obj is AppPreviewData {
+  return typeof obj == "object";
+}
 
 const Preview = async () => {
-  const data: PreviewData = previewData();
-  if (!data || !data.uuid) return notFound();
+  const data = previewData();
+  if (!isAppPreviewData(data)) return notFound();
   const uuid = data.uuid;
   const draft = await ghostRepo.getDraft(uuid).catch(console.error);
   if (!draft) return notFound();
