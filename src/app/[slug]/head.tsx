b03daa3a05a474ee2b8bae6cf10b08205e1e2b05
sunya9 <mugen.xyz@gmail.com>
2023-03-09 20:55:52 +0900

    feat: update next to v13.2.3

--- src/app/[slug]/head.tsx
+++ src/app/[slug]/head.tsx
@@ -1,14 +1,18 @@
+import { notFound } from "next/navigation";
+import { exclusionPageSlugs } from "../../lib/excludedPages";
 import { ghostRepo } from "../../lib/ghost";
 import { Canonical, Description } from "../../lib/head";
 
 export default async function Head({ params }: { params: { slug: string } }) {
-  const post = await ghostRepo.getPage(params.slug);
+  if (exclusionPageSlugs.includes(params.slug)) notFound();
+  const page = await ghostRepo.getPage(params.slug);
+  if (!page) notFound();
 
   return (
     <>
-      <title>{post.title}</title>
-      <Description description={post.custom_excerpt} />
-      <Canonical relativePath={post.slug} />
+      <title>{page.title}</title>
+      <Description description={page.custom_excerpt} />
+      <Canonical relativePath={page.slug} />
     </>
   );
 }
---
+++ src/lib/excludedPages.ts
@@ -0,0 +1,1 @@
+export const exclusionPageSlugs = ["about"];
