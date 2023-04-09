b007c727d981609b4c7a316bfb9d1738617ba02a
sunya9 <mugen.xyz@gmail.com>
2023-04-10 06:50:31 +0900

    fix: replace head.tsx with metadata API

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,10 +1,13 @@
+import { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { AppLayout } from "../../componments/AppLayout";
 import { Article } from "../../componments/Article";
 import { exclusionPageSlugs } from "../../lib/excludedPages";
 import { ghostRepo } from "../../lib/ghost";
 
-const Post = async ({ params }: { params: { slug: string } }) => {
+type Props = { params: { slug: string } };
+
+const Post = async ({ params }: Props) => {
   if (exclusionPageSlugs.includes(params.slug)) notFound();
   const page = await ghostRepo.getPage(params.slug);
   if (!page) notFound();
@@ -37,3 +40,18 @@ export async function generateStaticParams() {
     };
   });
 }
+
+export const metadata: (props: Props) => Promise<Metadata> = async ({
+  params,
+}) => {
+  if (exclusionPageSlugs.includes(params.slug)) notFound();
+  const page = await ghostRepo.getPage(params.slug);
+  if (!page) notFound();
+  return {
+    title: page.title,
+    description: page.custom_excerpt,
+    alternates: {
+      canonical: `/${page.slug}`,
+    },
+  };
+};
