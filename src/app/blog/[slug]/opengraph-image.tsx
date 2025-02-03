0f21895b77c4b40c76e968a3ae6a8dc040b9ada8
xyz <mugen.xyz@gmail.com>
2025-02-03 10:31:46 +0900

    feat: enhance metadata and SEO for blog pages

---
+++ src/app/blog/[slug]/opengraph-image.tsx
@@ -0,0 +1,22 @@
+import { notFound } from "next/navigation";
+import { OgImageTemplate } from "../../../componments/OgImageTemplate";
+import { ghostRepo } from "../../../lib/ghost";
+
+export const size = {
+  width: 1200,
+  height: 630,
+};
+
+export const contentType = "image/png";
+export default async function Image({
+  params: { slug },
+}: {
+  params: { slug: string };
+}) {
+  const post = await ghostRepo.getPost(slug);
+  if (!post) notFound();
+  return OgImageTemplate({
+    title: post.title,
+    date: post.published_at,
+  });
+}
---
+++ src/app/blog/opengraph-image.tsx
@@ -0,0 +1,13 @@
+import { OgImageTemplate } from "../../componments/OgImageTemplate";
+
+export const size = {
+  width: 1200,
+  height: 630,
+};
+
+export const contentType = "image/png";
+export default async function Image() {
+  return OgImageTemplate({
+    title: "全ての投稿",
+  });
+}
