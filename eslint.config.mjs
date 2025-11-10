765f5f1a13a4cfe993e0e6f44d813c338f784fb5
xyz <mugen.xyz@gmail.com>
2025-11-10 16:10:35 +0900

    chore(eslint): migrate ESLint configuration to new format and update linting command
    - Deleted the old `.eslintrc.json` file and created a new `eslint.config.mjs` file for improved configuration.
    - Updated the linting command in `package.json` to use `eslint .` instead of `next lint`.
    - Added `eslint-config-prettier` as a dependency for better integration with Prettier.

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier/flat";
+
+export default defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  prettier,
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+]);
