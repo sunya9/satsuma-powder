d20c686cc64026157d8393faae190760ad5b16b1
xyz <mugen.xyz@gmail.com>
2025-11-11 20:53:20 +0900

    refactor(fetch-config): remove RSS fetching from fetchConfig script and create dedicated RSS route
    Eliminated the RSS fetching functionality from the fetch-config script to streamline the process. Introduced a new route for fetching RSS, enhancing modularity and separation of concerns in the codebase.

--- scripts/fetch-config.mjs
+++ scripts/fetch-config.mjs
@@ -15,10 +15,4 @@ async function fetchConfig() {
   return pipeline(res.body, fs.createWriteStream("site.json"));
 }
 
-async function fetchRss() {
-  const res = await fetch(`${process.env.GHOST_URL}/rss`);
-  if (!res.ok) throw new Error("Failed to fetch RSS");
-  return pipeline(res.body, fs.createWriteStream("public/rss.xml"));
-}
-
-await Promise.all([fetchConfig(), fetchRss()]);
+await fetchConfig();
